299c2840321839a47475749315b288dc8daf3194
giladfuchs <igilfu@gmail.com>
2023-12-25 19:58:30 +0200

    update

--- leetcode/romanToInt.py
+++ leetcode/romanToInt.py
@@ -25,8 +25,35 @@ class Solution:
         roman_str = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
         print({i: j for j, i in roman_value.items()})
 
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows < 2:
+            return s
+        mat = [[] for _ in range(numRows)]
+        down = True
+        x, y = -1, 0
+        for c in s:
+            if down:
+                x += 1
+            else:
+                x -= 1
+                y += 1
+
+            if x == numRows - 1:
+                down = False
+            size = y - len(mat[x])
+            mat[x] += [''] * size + [c]
+            if not down and x == 1:
+                x = -1
+                down = True
+        ans = ''
+        for row in mat:
+            ans += ''.join(row)
+        return ans
+
+
+print(Solution().convert("PAYPALISHIRING", 3))
 
 # I can be placed before V (5) and X (10) to make 4 and 9.
 # X can be placed before L (50) and C (100) to make 40 and 90.
 # C can be placed before D (500) and M (1000) to make 400 and 90
-print(Solution().intToRoman("LVIII"))
+# print(Solution().intToRoman("LVIII"))
