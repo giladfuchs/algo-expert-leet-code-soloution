7f0e78779837c0b1e9291062bd2cac97e8771497
giladfuchs <igilfu@gmail.com>
2023-12-28 17:06:44 +0200

    update

--- leetcode/minOperations.py
+++ leetcode/minOperations.py
@@ -55,10 +55,9 @@ class Solution:
         if not s or s[0] == '0':
             return 0
 
-        n = len(s)
         prev, cur = 1, 1
 
-        for i in range(2, n + 1):
+        for i in range(2, len(s) + 1):
             one_digit = int(s[i - 1])
             two_digits = int(s[i - 2:i])
             temp = 0
@@ -72,8 +71,41 @@ class Solution:
 
         return cur
 
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        i, count = 0, 0
+        n = len(colors)
+        while i < n - 1:
+            if colors[i] != colors[i + 1]:
+                i += 1
+            else:
+                j = i + 1
+                while j < n - 1 and colors[j] == colors[j + 1]: j += 1
+                temp = neededTime[i:j + 1]
+                count += sum(temp) - max(temp)
+                i = j
+        return count
 
-print(Solution().numDecodings("11106"))
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        mod = 10 ** 9 + 7
+
+        if n * k < target:
+            return 0
+
+        dp = [[0] * (target + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+        for i in range(1, n + 1):
+            for j in range(i, min(i * k, target) + 1):
+                for temp in range(1, min(k, j) + 1):
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - temp]) % mod
+
+        return int(dp[n][target])
+
+
+# print(Solution().minCost(colors="abaac", neededTime=[1, 2, 3, 4, 5]))
+# print(Solution().minCost(colors="aabaa", neededTime=[1, 2, 3, 4, 1]))
+# print(Solution().numRollsToTarget(n=1, k=6, target=3))
+print(Solution().numRollsToTarget(n = 2, k = 6, target = 7))
+# print(Solution().numDecodings("11106"))
 # print(Solution().onesMinusZeros([[0, 1, 1], [1, 0, 1], [0, 0, 1]]))
 # print(Solution().isPathCrossing("NES"))
 # print(Solution().isPathCrossing("NESWW"))
