d37588849e3f2070cd7af17460ce27b6c3690889
giladfuchs <igilfu@gmail.com>
2024-01-01 19:02:55 +0200

    update

--- algoexpert/commonCharacters.py
+++ algoexpert/commonCharacters.py
@@ -15,6 +15,18 @@ def nonConstructibleChange(coins):
     return min_change + 1
 
 
+from collections import Counter
+
+
+def firstNonRepeatingCharacter(string):
+    count = Counter(string)
+    for i, c in enumerate(string):
+        if count[c] == 1:
+            return i
+    return -1
+
+
 if __name__ == '__main__':
-    print(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]))
+    print(firstNonRepeatingCharacter("abcdcaf"))
+    # print(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]))
     # print(commonCharacters(["abc", "bcd", "cbad"]))
